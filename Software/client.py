7bce6fcaa4c964d8704abbdc880f9d1798f5aa30
ivancornut <iv.cornut@gmail.com>
2024-05-17 16:07:07 +0100

    Increasing comments in host and client codes

--- Software/client.py
+++ Software/client.py
@@ -4,51 +4,50 @@ from machine import Pin, I2C
 from micropython_sht4x import sht4x
 import time, onewire, ds18x20, math, json
 
-ERROR_CODE = const(9999)
-REGISTER_HREG = const(93)
-COIL = const(123)
-
-SHT45 = False
-DS18B20 = False
+ERROR_CODE = const(9999) # error code when sensor data cannot be read
+REGISTER_HREG = const(93) # address of first register that contains the data
+COIL = const(123) # address of the sleep coil
 
 # Open the file containing parameters for this sensor node
 with open('client.json') as f:
     read_data = f.read()
 
-# Parse the json file output
-params = json.loads(read_data)
-client_address = params["GENERAL"]["Client_ID"]
-print("This is client nb "+str(client_address))
-nb_unique_sensors = params["GENERAL"]["Unique_Sensors"]
+params = json.loads(read_data) # Parse the json file output
+client_address = params["GENERAL"]["Client_ID"] # the client ID (its address on the ModBus network)
+print("This is client nb "+str(client_address)) # for debugging
+nb_unique_sensors = params["GENERAL"]["Unique_Sensors"] # The number of unique sensor types
 sensor_types = params["UNIQUE_SENSOR_TYPES"]
 if nb_unique_sensors != len(sensor_types): print("Error: Problem with sensor type number")
 
+# Setting up the type and number of sensors
 # For now only works with SHT45 and DS18B20 but can be expanded for other sensors
+SHT45 = False 
 if "SHT_45" in sensor_types:
     # Routine for extraction of information on present SHT45s
-    SHT45 = True
-    nb_SHT_45 = params["SHT_45"]["Nb"]
-    print("There are " + str(nb_SHT_45) + " SHTs on this client")
-    pins_sht45 = params["SHT_45"]["Sensor_Pins"]
-    if nb_SHT_45 != len(pins_sht45): print("Error: Problem with SHT45 number")
+    SHT45 = True # Tells the device to read sht45s
+    nb_SHT_45 = params["SHT_45"]["Nb"] # number of SHT45s
+    print("There are " + str(nb_SHT_45) + " SHTs on this client") # debugging
+    pins_sht45 = params["SHT_45"]["Sensor_Pins"] # get the I2C pins of the SHT45s
+    if nb_SHT_45 != len(pins_sht45): print("Error: Problem with SHT45 number") # debugging
     i2c = []
     for i in pins_sht45:
-        i2c.append(I2C(i["I2C"], sda = i["SDA"], scl = i["SCL"])) # Inititalising I2C connection 
+        i2c.append(I2C(i["I2C"], sda = i["SDA"], scl = i["SCL"])) # Creating the I2C objects
 
+DS18B20 = False
 if "DS18B20" in sensor_types:
     # Routine for extraction of information on present DS18B20s
-    DS18B20 = True
-    nb_DS18B20 = params["DS18B20"]["Nb"]
+    DS18B20 = True # Tells the device to read DS18B20s
+    nb_DS18B20 = params["DS18B20"]["Nb"] # Number of DS18B20s
     print("There are " + str(nb_DS18B20) + " DS18B20s on this client")
-    addresses = params["DS18B20"]["adrss"]
+    addresses = params["DS18B20"]["adrss"] # The individual addresses of the DS18B20s
     if nb_DS18B20 != len(addresses): print("Error: Problem with DS18B20 number")
-    ds_pin = Pin(params["DS18B20"]["Pin"])
-    ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+    ds_pin = Pin(params["DS18B20"]["Pin"]) # The GPIO pin on which DS18B20s are connected
+    ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin)) # creating the ds object
 
 led = Pin(25, Pin.OUT) # for debugging only
 
-def my_reg_set_cb(reg_type, address, val):
-    print('Custom callback, called on setting {} at {} to: {}'.format(reg_type, address, val))
+#def my_reg_set_cb(reg_type, address, val):
+#    print('Custom callback, called on setting {} at {} to: {}'.format(reg_type, address, val))
 
 def my_coil_set_cb(reg_type, address, val):
     """This function is to put the device to sleep following the retrieval of value by host
@@ -56,59 +55,60 @@ def my_coil_set_cb(reg_type, address, val):
     if val==0:
         time.sleep(2) # we sleep after the information has been read
         # Since this is only a time.sleep it is for debugging purposes
+        # machine.lightsleep() should be called for real world implementations
     else:
         print("No sleep") # in case something goes wrong
 
 def my_reg_get_cb(reg_type, address, val):
-    led.on()
+    led.on() # for debugging
     values = ()
     print("Holding register is being read")
     if SHT45:
         for i in i2c: # do the loop on the number of SHT45 devices connected
             try:
-                sht = sht4x.SHT4X(i)
-                time.sleep(0.1)
-                temperature, relative_humidity = sht.measurements
-                values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100))
+                sht = sht4x.SHT4X(i) # initialize sh45 objects
+                time.sleep(0.1) # give some time to device
+                temperature, relative_humidity = sht.measurements # read sensor
+                values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
             except:
-                try:
-                    sht = sht4x.SHT4X(i2c)
-                    sht.reset()
-                    time.sleep(0.1)
-                    temperature, relative_humidity = sht.measurements
-                    values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100))
+                try: # in case measurement is impossible try resetting
+                    sht = sht4x.SHT4X(i2c) # initialize sh45 objects
+                    sht.reset() # soft reset the SHT45
+                    time.sleep(0.1) # give some time to device
+                    temperature, relative_humidity = sht.measurements # read sensor
+                    values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
                 except:
-                    temperature = ERROR_CODE
-                    relative_humidity = ERROR_CODE
-                    values.append(math.trunc(temperature), math.trunc(relative_humidity))
+                    temperature = ERROR_CODE # if it goes wrong write 9999
+                    relative_humidity = ERROR_CODE # if it goes wrong write 9999
+                    values.append(math.trunc(temperature), math.trunc(relative_humidity)) # write to holding register so host device knows something is wrong
     if DS18B20: # Do the loop for number of DS18B20 connected (all on same pin since one-wire)
-        ds_sensor.convert_temp()
-        time.sleep_ms(750)
+        ds_sensor.convert_temp() # function to tell sensors to start measurement routine
+        time.sleep_ms(750) # time it takes sensors to read the temp
         for i in addresses:
             try:
-                values.append(math.trunc(ds_sensor.read_temp(rom))*100)
+                values.append(math.trunc(ds_sensor.read_temp(rom))*100) # reading values from sensor at address rom
             except:
                 values.append(ERROR_CODE)
-    client.set_hreg(address,values)
+    client.set_hreg(address,values) # Put all values inside the holding registers
     print('Custom callback, called on getting {} at {}, currently: {}'.format(reg_type, address, val))
     led.off()
 
-# Set the pins for communication
+# Set the pins for UART communication
 rtu_pins = (Pin(params["GENERAL"]["RTU_Pins"][0]), Pin(params["GENERAL"]["RTU_Pins"][1]))
 uart_id = params["GENERAL"]["UART_ID"]
 
-client = ModbusRTU(
+client = ModbusRTU( # Setup the ModBus object
     addr = client_address,
     pins = rtu_pins,
-    baudrate = 9600,
-    data_bits = 8,
-    stop_bits = 1,
-    parity = None,
-    ctrl_pin = 12,
+    baudrate = 9600, # important to keep consistent between host and client
+    data_bits = 8, # important to keep consistent between host and client
+    stop_bits = 1, # important to keep consistent between host and client
+    parity = None, # important to keep consistent between host and client
+    ctrl_pin = 12, # important to keep consistent between host and client
     uart_id = uart_id
     )
 
-register_definitions = {
+register_definitions = { # Setting up coils and registers
     "COILS": {"SLEEP_COIL": {"register": COIL,"len": 1,"val": 1, "on_get_cb": my_coil_set_cb}}, # coils give a status
     "HREGS": {"TEMPERATURE_and_HUM_HREG": {"register": REGISTER_HREG,"len": 2,"val": (9999,9999),"on_get_cb": my_reg_get_cb}}, # holding register for values
     "ISTS": {"EXAMPLE_ISTS": {"register": 67,"len": 1,"val": 0}},
--- Software/host.py
+++ Software/host.py
@@ -5,56 +5,58 @@ import time, json
 # Open the file containing parameters for this sensor node
 with open('host.json') as f:
     read_data = f.read()
-# Parse the json file output
-params = json.loads(read_data)
 
-sleep_time = params["GENERAL"]["Time_Delta"]*60 * 1000 - 60*1000
-# Count the number of clients and the number of values to retrieve per client
-nb_clients = params["GENERAL"]["Client_nb"]
+params = json.loads(read_data) # Parse the json file output
+
+sleep_time = params["GENERAL"]["Time_Delta"]*60 * 1000 - 60*1000 # the Time during which the host should sleep (it leaves 1min for execution of tasks)
+
+nb_clients = params["GENERAL"]["Client_nb"] # Count the number of clients and the number of values to retrieve per client
 nb_values = [] # number of values per client
 print(params)
 for i in range(1, nb_clients+1):
      nb_values.append(params["CLIENT_"+str(i)]["nb_values"])
-if len(nb_values) != nb_clients: print("Error on nb of clients")
+if len(nb_values) != nb_clients: print("Error on nb of clients") # check consistency
 
 # retrieve the UART Pins
-rtu_pins = (Pin(params["GENERAL"]["RTU_Pins"][0]), Pin(params["GENERAL"]["RTU_Pins"][1]))
-uart_id = params["GENERAL"]["UART_ID"]
+rtu_pins = (Pin(params["GENERAL"]["RTU_Pins"][0]), Pin(params["GENERAL"]["RTU_Pins"][1])) # the GPIO pins
+uart_id = params["GENERAL"]["UART_ID"] # The UART ID, see Pico Pinout
 
 led = Pin(25, Pin.OUT)
 
-host = ModbusRTUMaster(
+host = ModbusRTUMaster( # setup the Modbus object
     pins = rtu_pins,
-    baudrate = 9600,
-    data_bits = 8,
-    stop_bits = 1,
-    parity = None,
-    ctrl_pin = 12,
+    baudrate = 9600, # important to keep consistent between host and client
+    data_bits = 8, # important to keep consistent between host and client
+    stop_bits = 1, # important to keep consistent between host and client
+    parity = None, # important to keep consistent between host and client
+    ctrl_pin = 12, # important to keep consistent between host and client
     uart_id = uart_id
     )
 
 while True:
     with open('data.txt','a') as f:
         for i in range(1,nb_clients+1):
-            f.write(str(i))
+            f.write(str(i)) # write number of device in data file
             try:
-                
+                # Reading the holding registers of the client device
+                # Starts at the starting address and until the starting address + register_qty
                 data = host.read_holding_registers(slave_addr=i, starting_addr=93, register_qty=nb_values[i])
-                for d in data:
-                    f.write(","+str(d))
-                print('Value of Holding register 93: {}'.format(data))
+                for d in data: # iterate over data array
+                    f.write(","+str(d)) # write each value in csv file
+                print('Value of Holding register 93: {}'.format(data)) # for debugging
                 try:
+                    # Putting device to sleep after succefull reading of sensor values
                     host.write_single_coil(slave_addr=i, output_address=123, output_value = 0)
                     print("Successfully set sleeping coil")
                 except:
                     print("Unsuccessful at setting sleepin coil")
                 led.off()
+                f.write("\n") # prepare for next device
                 time.sleep(0.25)
-                f.write("\n")
             except:
                 print("Problem with Client n°: "+str(i))
-                f.write(",1,1,1" + "\n")
+                f.write(",1,1,1" + "\n") # write false data
                 led.on()
                 time.sleep(0.25)
-    machine.lightsleep(sleep_time)
+    machine.lightsleep(sleep_time) # put host to sleep for an amount of time to save power
     
