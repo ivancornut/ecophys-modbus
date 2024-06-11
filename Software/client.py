c75ff6e5d4b9a2c5b1551d8815f27865cd160c8f
ivancornut <iv.cornut@gmail.com>
2024-06-11 10:47:41 +0100

    Modified client and host datalogger micropythons to correct bugs

--- Software/client.py
+++ Software/client.py
@@ -5,9 +5,12 @@ from micropython_sht4x import sht4x
 import time, onewire, ds18x20, math, json
 import ads1x15
 
+time.sleep(2)
 ERROR_CODE = const(9999) # error code when sensor data cannot be read
 REGISTER_HREG = const(93) # address of first register that contains the data
 COIL = const(123) # address of the sleep coil
+
+register_length = 0
 
 # Open the file containing parameters for this sensor node
 with open('client.json') as f:
@@ -27,6 +30,7 @@ if "SHT_45" in sensor_types:
     # Routine for extraction of information on present SHT45s
     SHT45 = True # Tells the device to read sht45s
     nb_SHT_45 = params["SHT_45"]["Nb"] # number of SHT45s
+    register_length = register_length + nb_SHT_45*2
     print("There are " + str(nb_SHT_45) + " SHTs on this client") # debugging
     pins_sht45 = params["SHT_45"]["Sensor_Pins"] # get the I2C pins of the SHT45s
     if nb_SHT_45 != len(pins_sht45): print("Error: Problem with SHT45 number") # debugging
@@ -39,74 +43,82 @@ if "DS18B20" in sensor_types:
     # Routine for extraction of information on present DS18B20s
     DS18B20 = True # Tells the device to read DS18B20s
     nb_DS18B20 = params["DS18B20"]["Nb"] # Number of DS18B20s
+    register_length = register_length + nb_DS18B20
     print("There are " + str(nb_DS18B20) + " DS18B20s on this client")
     addresses = params["DS18B20"]["adrss"] # The individual addresses of the DS18B20s
     if nb_DS18B20 != len(addresses): print("Error: Problem with DS18B20 number")
     ds_pin = Pin(params["DS18B20"]["Pin"]) # The GPIO pin on which DS18B20s are connected
     ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin)) # creating the ds object
 
-Dendro = True
+Dendrometer = False
 if "Dendrometer" in sensor_types:
     # Routine for reading the outputs of ADC reading dendrometers
     addr = 72
     gain = 1
-    Dendro = True
+    Dendrometer = True
     nb_dendro = params["Dendrometer"]["Nb"] # Number of dendromets
+    register_length = register_length + nb_dendro
     activation_Pin = params["Dendrometer"]["Activation_Pin"]
     print("There are " + str(nb_dendro) + " dendrometers on this client")
-    pins_ADC = params["Dendros"]["Sensor_Pins"] # I2C pins of the ADC
+    pins_ADC = params["Dendrometer"]["Sensor_Pins"] # I2C pins of the ADC
     i2c_adc = I2C(pins_ADC["I2C"], sda = pins_ADC["SDA"], scl = pins_ADC["SCL"]) # creating I2C objects for the sensors
     p0 = Pin(activation_Pin, Pin.OUT) # pin to power the lvdt
     p0.value(0) # start by turning it off
 
 led = Pin(25, Pin.OUT) # for debugging only
 
+register_starting_values = (0,) * register_length
+
+values = register_starting_values
 #def my_reg_set_cb(reg_type, address, val):
 #    print('Custom callback, called on setting {} at {} to: {}'.format(reg_type, address, val))
 
 def my_coil_set_cb(reg_type, address, val):
     """This function is to put the device to sleep following the retrieval of value by host
     while maintaining the decision of host on sleep (the host sets the coil to 0 for sleep"""
-    if val==0:
-        time.sleep(2) # we sleep after the information has been read
+    global values
+    time_step_s = client.get_hreg(250)
+    if val:
+        print("Sleeping coil set to sleep for " + str(time_step_s) + " s")
+        time.sleep(0.25)
+        if time_step_s>61:
+            machine.lightsleep(time_step_s*1000 - 60*1000) # we sleep after the information has been read
+        else:
+            machine.lightsleep(time_step_s*1000 - 3000)
         # Since this is only a time.sleep it is for debugging purposes
         # machine.lightsleep() should be called for real world implementations
     else:
         print("No sleep") # in case something goes wrong
-
-def my_reg_get_cb(reg_type, address, val):
-    led.on() # for debugging
     values = ()
-    print("Holding register is being read")
     if SHT45:
         for i in i2c: # do the loop on the number of SHT45 devices connected
             try:
                 sht = sht4x.SHT4X(i) # initialize sh45 objects
                 time.sleep(0.1) # give some time to device
                 temperature, relative_humidity = sht.measurements # read sensor
-                values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
+                values = values + (math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
             except:
                 try: # in case measurement is impossible try resetting
                     sht = sht4x.SHT4X(i2c) # initialize sh45 objects
                     sht.reset() # soft reset the SHT45
                     time.sleep(0.1) # give some time to device
                     temperature, relative_humidity = sht.measurements # read sensor
-                    values.append(math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
+                    values = values + (math.trunc(temperature*100), math.trunc(relative_humidity*100)) # write to register (x100 for floats to int)
                 except:
                     temperature = ERROR_CODE # if it goes wrong write 9999
                     relative_humidity = ERROR_CODE # if it goes wrong write 9999
-                    values.append(math.trunc(temperature), math.trunc(relative_humidity)) # write to holding register so host device knows something is wrong
+                    values = values + (math.trunc(temperature), math.trunc(relative_humidity)) # write to holding register so host device knows something is wrong
     if DS18B20: # Do the loop for number of DS18B20 connected (all on same pin since one-wire)
         ds_sensor.convert_temp() # function to tell sensors to start measurement routine
         time.sleep_ms(750) # time it takes sensors to read the temp
         for i in addresses:
             try:
-                values.append(math.trunc(ds_sensor.read_temp(rom))*100) # reading values from sensor at address rom
+                values = values + (math.trunc(ds_sensor.read_temp(rom))*100) # reading values from sensor at address rom
             except:
-                values.append(ERROR_CODE)
+                values = values+(ERROR_CODE)
     if Dendrometer:
         try:
-            ads = ads1x15.ADS1115(i2c, addr, gain)
+            ads = ads1x15.ADS1115(i2c_adc, addr, gain)
             try:
                 p0.on()
                 value1 = 0
@@ -114,19 +126,28 @@ def my_reg_get_cb(reg_type, address, val):
                 for i in range(0,10):
                     value1 = value1 + ads.read(1,0)/10
                     value2 = value2 + ads.read(1,1)/10
-                    sleep(0.1)
+                    time.sleep(0.1)
         
                 voltage1 = ads.raw_to_v(value1)
                 voltage2 = ads.raw_to_v(value2)
                 ratio = voltage2/voltage1
-            except:
-                ratio = 9999
-            values.append(math.trunc(ratio*65000))
-        except:
-            values.append(9999)
+                print(ratio)
+            except Exception as error:
+                print(error)
+                ratio = 0.99
+            print(math.trunc(ratio*65000))
+            values = values+(math.trunc(ratio*65000),)
+        except Exception as error:
+            print(error)
+            values = values + (9999,)
         p0.off()
+    print(values)
+
+def my_reg_get_cb(reg_type, address, val):
+    led.on() # for debugging
+    print("Holding register is being read")
     client.set_hreg(address,values) # Put all values inside the holding registers
-    print('Custom callback, called on getting {} at {}, currently: {}'.format(reg_type, address, val))
+    print('Custom callback, called on getting {} at {}, currently: {}'.format(reg_type, address, values))
     led.off()
 
 # Set the pins for UART communication
@@ -145,14 +166,16 @@ client = ModbusRTU( # Setup the ModBus object
     )
 
 register_definitions = { # Setting up coils and registers
-    "COILS": {"SLEEP_COIL": {"register": COIL,"len": 1,"val": 1, "on_get_cb": my_coil_set_cb}}, # coils give a status
-    "HREGS": {"TEMPERATURE_and_HUM_HREG": {"register": REGISTER_HREG,"len": 2,"val": (9999,9999),"on_get_cb": my_reg_get_cb}}, # holding register for values
+    "COILS": {"SLEEP_COIL": {"register": COIL,"len": 1,"val": 0, "on_set_cb": my_coil_set_cb}}, # coils give a status
+    "HREGS": {"TEMPERATURE_and_HUM_HREG": {"register": REGISTER_HREG,"len": register_length,"val": register_starting_values,"on_get_cb": my_reg_get_cb},
+              "SLEEP_TIME_HREG":{"register":250, "len":1, "val":5}}, # holding register for values
     "ISTS": {"EXAMPLE_ISTS": {"register": 67,"len": 1,"val": 0}},
-    "IREGS": {"EXAMPLE_IREG": {"register": 10,"len": 1,"val": 60001}}
+    "IREGS": {"Times_step": {"register": 10,"len": 1,"val": 60001}}
 }
 
 # use the defined values of each register type provided by register_definitions
 client.setup_registers(registers=register_definitions)
+
 while True:
     try:
         result = client.process()
--- Software/datalogger/datalogger_host.py
+++ Software/datalogger/datalogger_host.py
@@ -1,15 +1,44 @@
 from umodbus.serial import Serial as ModbusRTUMaster
-from machine import Pin
+from machine import Pin, I2C, SPI
 import time, json
+import urtc
+import vfs
+import sdcard
 
+def error_data_string(nb=1):
+    # this function is to fill the datatable if no
+    # information is received from the device
+    datafmted = ""
+    for i in range(1,nb+1):
+        datafmted = datafmted+","+str(999)
+    return datafmted
+
+time.sleep(5)
 # Open the file containing parameters for this sensor node
 with open('host.json') as f:
     read_data = f.read()
 
-time.sleep(10)
+
 params = json.loads(read_data) # Parse the json file output
 
-sleep_time = params["GENERAL"]["Time_Delta"]*60 * 1000 - 50*1000 # the Time during which the host should sleep (it leaves 1min for execution of tasks)
+# Setup cs pin for SD SPI$
+cs = Pin(17,Pin.OUT)
+# setup SPI for the SD card
+spi = machine.SPI(0,
+                  baudrate=1000000,
+                  polarity=0,
+                  phase=0,
+                  bits=8,
+                  firstbit=machine.SPI.MSB,
+                  sck=Pin(18),
+                  mosi=Pin(19),
+                  miso=Pin(16))
+sd = sdcard.SDCard(spi, cs)
+filsys = vfs.VfsFat(sd)
+
+sleep_time = params["GENERAL"]["Time_Delta"]*60 * 1000 - 30*1000 # the Time during which the host should sleep (it leaves 1min for execution of tasks)
+sleep_time_client_s = params["GENERAL"]["Time_Delta"]*60
+sleep_time_minutes = params["GENERAL"]["Time_Delta"]
 
 nb_clients = params["GENERAL"]["Client_nb"] # Count the number of clients and the number of values to retrieve per client
 nb_values = [] # number of values per client
@@ -17,6 +46,10 @@ print(params)
 for i in range(1, nb_clients+1):
      nb_values.append(params["CLIENT_"+str(i)]["nb_values"])
 if len(nb_values) != nb_clients: print("Error on nb of clients") # check consistency
+
+# Setup rtc to read it and save in data
+i2c_clock = I2C(0,scl=Pin(5), sda=Pin(4))
+rtc = urtc.PCF8523(i2c_clock)
 
 # retrieve the UART Pins
 rtu_pins = (Pin(params["GENERAL"]["RTU_Pins"][0]), Pin(params["GENERAL"]["RTU_Pins"][1])) # the GPIO pins
@@ -35,20 +68,42 @@ host = ModbusRTUMaster( # setup the Modbus object
     )
 
 while True:
-    with open('data.txt','a') as f:
+
+    
+    try:
+        SD = True
+        vfs.mount(filsys, "/sd")
+        file = '/sd/data.txt'
+    except:
+        print("couldn't mount SD card")
+        SD = False
+        file = 'data.txt'
+    while (rtc.datetime().minute % sleep_time_minutes) != 0:
+        #this is the time at which data is retieved
+        time.sleep(10)
+    for i in range(1, nb_clients+1):
+        try:
+            host.write_single_register(slave_addr=i, register_address=250, register_value = sleep_time_client_s, signed = False)
+        except:
+            print("Could not set sleep time at client "+str(i))
+    with open(file,'a') as f:
         for i in range(1,nb_clients+1):
-            f.write(str(i)) # write number of device in data file
+            datetime = rtc.datetime()
+            f.write(str(datetime.year)+","+str(datetime.month)+","+str(datetime.day)+","+str(datetime.hour)+","+str(datetime.minute)+","+str(datetime.second))
+            f.write(","+str(i)) # write number of device in data file
             try:
                 # Reading the holding registers of the client device
                 # Starts at the starting address and until the starting address + register_qty
+                print(nb_values[i-1])
                 data = host.read_holding_registers(slave_addr=i, starting_addr=93, register_qty=nb_values[i-1])
                 for d in data: # iterate over data array
                     f.write(","+str(d)) # write each value in csv file
+                print("DateTime", str(datetime.minute))
                 print('Value of Holding register 93: {}'.format(data)) # for debugging
                 time.sleep(0.25)
                 try:
                     # Putting device to sleep after succefull reading of sensor values
-                    host.write_single_coil(slave_addr=i, output_address=123, output_value = 0)
+                    host.write_single_coil(slave_addr=i, output_address=123, output_value = 1)
                     print("Successfully set sleeping coil")
                 except Exception as error:
                     print("Unsuccessful at setting sleepin coil")
@@ -59,9 +114,11 @@ while True:
             except Exception as error:
                 print("Problem with Client n°: "+str(i))
                 print(error)
-                f.write(",1,1,1" + "\n") # write false data
+                f.write(error_data_string(nb=nb_values[i-1]) + "\n") # write false data
                 led.on()
                 time.sleep(0.25)
-    #machine.lightsleep(sleep_time) # put host to sleep for an amount of time to save power
-    time.sleep(10)
+    if SD:
+        vfs.umount("/sd")
+    machine.lightsleep(sleep_time) # put host to sleep for an amount of time to save power
+    #time.sleep(10)
     
